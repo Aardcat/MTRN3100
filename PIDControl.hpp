bc5233795a03b5630fbdaae055c268e6cb1d8ae5
Nandit <nandit.shah2004@gmail.com>
2026-08-16 20:11:13 +1000

    Add forTurning parameter to PIDController constructor

--- PIDControl.hpp
+++ PIDControl.hpp
@@ -6,7 +6,7 @@ namespace mtrn3100 {
 
 class PIDController {
 public:
-    PIDController(float kp, float ki, float kd, long tolerance) : kp(kp), ki(ki), kd(kd), tolerance(tolerance) {}
+    PIDController(float kp, float ki, float kd, long tolerance, bool forTurning) : kp(kp), ki(ki), kd(kd), tolerance(tolerance), forTurning(forTurning) {}
 
     // Compute the output signal required from the current/actual value.
     float compute(float input) {
@@ -16,6 +16,12 @@ public:
 
         error = setpoint - (input - zero_ref);
         /*
+        if (forTurning) {
+            if (error > 180) {  error -= 180; error = error * -1;  }
+            else if (error < -180) {    error += 180; error = error * -1;   }
+        }
+        */
+        /*
         Serial.print("Error is: ");
         Serial.println(error);
         */
@@ -24,6 +30,7 @@ public:
 			output = 0;
 			return output;
 		}
+
 
         integral = integral + error * dt;
         derivative = (error-prev_error) / dt;
@@ -67,8 +74,8 @@ private:
     float prev_error = 0;
     float setpoint = 0;
     float zero_ref = 0;
+    bool forTurning;
 
-    
 };
 
 }  // namespace mtrn3100
